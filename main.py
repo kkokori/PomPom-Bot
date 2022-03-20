9c55d3fd21c25626a43ece0f8c9231ac37a95afe
Rebekah Salsburg <korineko18@yahoo.com>
2022-03-19 20:59:33 -0400

    created text file to save reminders in case of bot reset saves and reads txt reminders sends late reminder messages

--- main.py
+++ main.py
@@ -1,22 +1,164 @@
-import discord
 import asyncio
+from datetime import datetime, timedelta
+import discord
 import os
 from threading import Timer
 from dotenv import load_dotenv
 load_dotenv()
 
+# various valid time calls
+seconds = ['s', 'sec', 'secs', 'second', 'seconds']
+minutes = ['m', 'min', 'mins', 'minute', 'minutes']
+hours = ['h', 'hr', 'hrs', 'hour', 'hours']
+days = ['d', 'day', 'days']
+months = ['M', 'mo', 'month', 'months']
+years = ['y', 'yr', 'yrs', 'year', 'years']
 
 client = discord.Client()
+debug = False
 
 commands = {
     "!help": "list commands",
     "!events": "list events",
-    "!add": "<event-name>",
-    }
+    "!add": "<date> <time> <location> <description>",
+}
+
 
 @client.event
 async def on_ready():
+    await parse_reminder_list()
     print('Logged in at {0.user}'.format(client))
+
+
+# convert date values to seconds
+def to_seconds(num, t):
+    if t in seconds:
+        return num
+    elif t in minutes:
+        return num * 60
+    elif t in hours:
+        return num * 3600
+    elif t in days:
+        return num * 86400
+    elif t in months:
+        return num * 2629800
+    elif t in years:
+        return num * 31556952
+
+
+async def remind(remindIn, reminderMsg, replyMsg, mentionReply, channel):
+    def remove_date(date):
+        with open("reminders.txt", "r") as file:
+            lines = file.readlines()
+            file.close()
+
+        with open("reminders.txt", "w") as file:
+            flag = False
+            for line in lines:
+                if flag:
+                    flag = False
+                elif not line.startswith(str(date)):
+                    file.write(line)
+                else:
+                    flag = True
+            file.close()
+        return
+
+
+    delta = remindIn - datetime.now()
+    secondsToRemind = delta.total_seconds()
+   
+    await asyncio.sleep(secondsToRemind)
+    await channel.send(reminderMsg, reference=replyMsg, mention_author=mentionReply)
+    remove_date(remindIn.isoformat())
+
+
+async def parse_reminder_list():
+    with open("reminders.txt", "r") as file:
+        lines = file.readlines()
+        file.close()
+
+    with open("reminders.txt", "w") as file:
+        firstFlag = True
+        for line in lines:
+            file.write(line)
+            # first line: date, reminderMsg p1
+            if (firstFlag):
+                lineInfo = line.split("`~")
+                remindDate = datetime.fromisoformat(lineInfo[0].strip())
+                reminderMsg = lineInfo[1]
+                firstFlag = False
+            else: # second line: remindMsg p2, replyMsg, mentionReply, origMsg
+                lineInfo = line.split("`~")
+                reminderMsg += lineInfo[0]
+                mentionReply = lineInfo[2]
+                print(lineInfo[3])
+                channel = await client.fetch_channel(lineInfo[3])             
+                replyMsg = await channel.fetch_message(lineInfo[1])
+
+                # reminder time already passed
+                if remindDate < datetime.now(): 
+                    reminderMsg = "Uh oh. We might have missed a reminder for " + remindDate.strftime("%B %d, %Y at %I:%M %p") + ".\n" + lineInfo[0]
+                
+                await remind(remindDate, reminderMsg, replyMsg, mentionReply, channel)
+                firstFlag = True
+
+        file.close()
+        return
+
+
+
+async def new_remind(message, pts):
+    # parse command
+    num = pts[1]  # number value
+    denom = pts[2]  # time denomination
+
+    if num.isnumeric() == False:
+        await message.channel.send("Invalid number value. Ensure the second argument is a number.")
+        return
+    elif denom not in (seconds + minutes + hours + days + months + years):
+        await message.channel.send("Invalid time format. Valid formats are:\n```seconds: " + str(seconds[0:]) + "\nminutes: " + str(minutes[0:]) + "\nhours: " + str(hours[0:]) + "\ndays: " + str(days[0:]) + "\nmonths: " + str(months[0:]) + "\nyears: " + str(years[0:]) + "```")
+        return
+
+    
+    # get the seconds difference and find the date of reminding
+    secondsTilRemind = to_seconds(int(num), denom)
+    remindDate = datetime.now() + timedelta(seconds=secondsTilRemind)
+
+    mentionUser = message.author.mention
+    reminderMsg = "Reminding you, " + mentionUser + "."
+
+    # check if there's a note
+    reminderNote = "\n"
+    if len(pts) > 3:
+        reminderNote += "> " + (" ".join(pts[3:]))
+    reminderMsg += reminderNote
+
+    # confirmation of reminder
+    await message.add_reaction('✅')
+
+    # reply to remind message if the remind message is not replying to a different message
+    replyMsg = message.reference if message.reference else message
+
+    # do not mention user if the remind message is replying to a different message
+    mentionReply = False if message.reference else True
+    
+    channel = message.channel
+  
+    # save the whole reminder
+    def saveReminder():
+        # remind date, remind note/msg, message to reply to, ping on/off, original message
+        file = open("reminders.txt", "a")
+        file.write(remindDate.isoformat() + "`~"+ str(reminderMsg) + "`~" +
+                   str(replyMsg.id) + "`~" + str(mentionReply) + "`~" + str(channel.id) + "\n")
+        file.close()
+        return
+    saveReminder()
+
+    # set the remind
+    await remind(remindDate, reminderMsg,
+           replyMsg, mentionReply, channel)
+
 
 @client.event
 async def on_message(message):
@@ -25,63 +167,25 @@ async def on_message(message):
 
     pts = message.content.split(" ")
     cmd = pts[0]
-    print(cmd)
+    channel = message.channel
+    if debug:
+        print(cmd)
 
     match cmd:
         case '!help':
-            await message.channel.send("```!help - list commands\n\n!events - list event\n!add <event name> <date> <time> <location>\n\n!remind <#> <sec/min/hr/day/mo/yr>```")
+            await channel.send("```!help - list commands\n\n!events - list event\n!add <date> <time> <location> <desc>\n\n!remind <#> <sec/min/hr/day/mo/yr>```")
+
         case '!remind':
-            print(message.author)
             if len(pts) < 3:
-                await message.channel.send("Invalid format. Use `!remind <#> <sec/min/hr/day/mo/yr> <optional message>`")
-            
-            num = pts[1] # number value
-            denom = pts[2] # time denomination
+                await channel.send("Invalid format. Use `!remind <#> <sec/min/hr/day/mo/yr> <optional message>`")
+            await new_remind(message, pts)
 
-            # various valid time calls
-            seconds = ['s', 'sec', 'secs', 'second', 'seconds']
-            minutes = ['m', 'min', 'mins', 'minute', 'minutes']
-            hours = ['h', 'hr', 'hrs', 'hour', 'hours']
-            days = ['d', 'day', 'days']
-            months = ['M', 'mo', 'month', 'months']
-            years = ['y', 'yr', 'yrs', 'year', 'years']
+        case '!add':
+            date = pts[1]
+            time = pts[2]
+            place = pts[3]
+            desc = pts[4:]
 
-            # converting values to seconds for asyncio
-            def toSeconds(num, t):
-                if t in seconds:
-                    return num
-                elif t in minutes:
-                    return num * 60
-                elif t in hours:
-                    return num * 3600
-                elif t in days:
-                    return num * 86400
-                elif t in months:
-                    return num * 2629800
-                elif t in years:
-                    return num * 31,556,952
-
-            if num.isnumeric() == False:
-                await message.channel.send("Invalid number value. Ensure the second argument is a number.")
-            elif denom not in (seconds + minutes + hours + days + months + years):
-                await message.channel.send("Invalid time format. Valid formats are:\n```seconds: " + str(seconds[0:]) + "\nminutes: " + str(minutes[0:]) + "\nhours: " + str(hours[0:]) + "\ndays: " + str(days[0:]) + "\nmonths: " + str(months[0:]) + "\nyears: "+ str(years[0:]) +"```")
-            else: 
-                remindIn = toSeconds(int(num), denom)
-                mentionUser = message.author.mention
-                reminderMsg = "Reminding you, " + mentionUser
-            
-                print(len(pts))
-                if len(pts) > 3:
-                    reminderMsg += ": " + (" ".join(pts[3:]))
-                
-                # reply to remigignd message if the remind message is not replying to a different message
-                replyMsg = message.reference if message.reference else message
-                # do not mention user if the remind message is replying to a different message
-                mentionReply = False if message.reference else True
-                
-                await asyncio.sleep(remindIn)
-                await message.channel.send(reminderMsg, reference=replyMsg, mention_author=mentionReply)
-                
         case '!bojji':
             id = 0
             for e in message.guild.emojis:
@@ -89,16 +193,19 @@ async def on_message(message):
                     id = e.id
                     break
 
-            await message.channel.send("<:bojji:" + str(id) + ">")
+            await channel.send("<:bojji:" + str(id) + ">")
         case _:
             print(message.content)
             if cmd.startswith('!'):
-                await message.channel.send("Invalid command. `!help` for command list.")
+                await channel.send("Invalid command. `!help` for command list.")
 
 
 is_prod = os.environ.get('IS_HEROKU', None)
 
+# handle reading token when run locally vs on heroku
 if is_prod:
     client.run(os.environ.get('TOKEN'))
 else:
     client.run(os.getenv('TOKEN'))
+
+# read in the old reminders and store/run them
